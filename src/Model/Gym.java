cfc1e732e9fea83ef6eeddeb88995d2eecf63375
winhtuan <winhtuan@gmail.com>
2024-09-28 18:08:48 +0700

    create obj

--- src/Model/Gym.java
+++ src/Model/Gym.java
@@ -1,6 +1,6 @@
 package Model;
 
-import Reponsitory.FileManager;
+import Repository.FileManager;
 import View.Validation;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -17,41 +17,42 @@ public class Gym {
         courses = new HashMap<>();
         fileManager = new FileManager();
         valid = new Validation();
+
         // Create Workout object for each Course
-        Workout workoutBung = new Workout(
-                "Tập trung vào cơ bụng",
+        Workout absWorkout = new Workout(
+                "Focus on abdominal muscles",
                 new HashSet<>(Arrays.asList("Crunches", "Leg Raise", "Plank")),
                 Arrays.asList("Low carb", "Protein rich meals"),
                 Map.of("Monday", Arrays.asList("Crunches", "Leg Raise"),
                         "Wednesday", Arrays.asList("Plank", "Bicycle Crunch"))
         );
 
-        Workout workoutChan = new Workout(
-                "Tập trung vào cơ chân",
+        Workout legWorkout = new Workout(
+                "Focus on leg muscles",
                 new HashSet<>(Arrays.asList("Squats", "Lunges", "Leg Press")),
                 Arrays.asList("Balanced diet", "High fiber meals"),
                 Map.of("Tuesday", Arrays.asList("Squats", "Lunges"),
                         "Thursday", Arrays.asList("Leg Press", "Calf Raises"))
         );
 
-        Workout workoutTay = new Workout(
-                "Tập trung vào cơ tay",
+        Workout armWorkout = new Workout(
+                "Focus on arm muscles",
                 new HashSet<>(Arrays.asList("Push-ups", "Bicep Curls", "Tricep Dips")),
                 Arrays.asList("High protein", "Healthy fats"),
                 Map.of("Wednesday", Arrays.asList("Push-ups", "Bicep Curls"),
                         "Friday", Arrays.asList("Tricep Dips", "Pull-ups"))
         );
 
-        Workout workoutCalo = new Workout(
-                "Đốt cháy calo",
+        Workout calorieBurningWorkout = new Workout(
+                "Burn calories",
                 new HashSet<>(Arrays.asList("Running", "Jump Rope", "Burpees")),
                 Arrays.asList("Balanced meals", "High fiber, low sugar"),
                 Map.of("Monday", Arrays.asList("Running", "Jump Rope"),
                         "Friday", Arrays.asList("Burpees", "Cycling"))
         );
 
-        Workout workoutYoga = new Workout(
-                "Thư giãn và dẻo dai với Yoga",
+        Workout yogaWorkout = new Workout(
+                "Relax and increase flexibility with Yoga",
                 new HashSet<>(Arrays.asList("Sun Salutation", "Tree Pose", "Warrior Pose")),
                 Arrays.asList("Detox diet", "Vegetarian meals"),
                 Map.of("Sunday", Arrays.asList("Sun Salutation", "Tree Pose"),
@@ -66,25 +67,25 @@ public class Gym {
         Coach coachYoga = new Coach("Coach E", "Yoga");
 
         // Create Course objects
-        Course courseAbs = new Course("C01", "Abs Course", 500.0, "2 months", coachAbs, workoutBung);
-        Course courseLegs = new Course("C02", "Legs Course", 450.0, "2 months", coachLegs, workoutChan);
-        Course courseArms = new Course("C03", "Arms Course", 400.0, "1.5 months", coachArms, workoutTay);
-        Course courseCalories = new Course("C04", "Calorie-Burning Course", 550.0, "3 months", coachCalories, workoutCalo);
-        Course courseYoga = new Course("C05", "Yoga Course", 600.0, "3 months", coachYoga, workoutYoga);
+        Course absCourse = new Course("C01", "Abs Course", 500.0, "2 months", coachAbs, absWorkout);
+        Course legCourse = new Course("C02", "Legs Course", 450.0, "2 months", coachLegs, legWorkout);
+        Course armCourse = new Course("C03", "Arms Course", 400.0, "1.5 months", coachArms, armWorkout);
+        Course calorieBurningCourse = new Course("C04", "Calorie-Burning Course", 550.0, "3 months", coachCalories, calorieBurningWorkout);
+        Course yogaCourse = new Course("C05", "Yoga Course", 600.0, "3 months", coachYoga, yogaWorkout);
 
-        courses.put(courseAbs.getCourseId(), courseAbs);
-        courses.put(courseLegs.getCourseId(), courseLegs);
-        courses.put(courseArms.getCourseId(), courseArms);
-        courses.put(courseCalories.getCourseId(), courseCalories);
-        courses.put(courseYoga.getCourseId(), courseYoga);
+        courses.put(absCourse.getCourseId(), absCourse);
+        courses.put(legCourse.getCourseId(), legCourse);
+        courses.put(armCourse.getCourseId(), armCourse);
+        courses.put(calorieBurningCourse.getCourseId(), calorieBurningCourse);
+        courses.put(yogaCourse.getCourseId(), yogaCourse);
     }
-    // hàm main test code
+
+    // main method to test the code
     public static void main(String[] args) {
         Gym g = new Gym();
-        System.out.println("List Courses:");
+        System.out.println("List of Courses:");
         g.courses.forEach((key, value) -> {
             System.out.println("ID: " + key + ", Course Name: " + value.getCourseName());
         });
     }
-
 }
--- src/Repository/FileManager.java
+++ src/Repository/FileManager.java
@@ -1,4 +1,4 @@
-package Reponsitory;
+package Repository;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
